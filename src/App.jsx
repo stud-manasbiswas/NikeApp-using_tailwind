2c2928d9d50d6ba3122c69dd1076136547f7b3c6
stud-manasbiswas <manasbiswas342@gmail.com>
2025-06-26 23:22:37 +0530

    Temporary rename to fix casing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
-import './index.css' // Ensure Tailwind CSS is applied
+import './index.css'
 
-function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-amber-400">
-      <h1 className="text-4xl font-bold text-white drop-shadow-lg">
-        Hello from Manas 🚀
-      </h1>
-    </div>
-  )
-}
+import {
+  HeroSection,
+  PopularProducts,
+  CustomerReview,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+  Footer
+} from './sections'
+
+import Nav from './Components/Nav'
+
+const App = () => (
+  <main className='relative'>
+    <Nav />
+
+    <section className='xl:padding-1 wide:padding-r padding-b'>
+      <HeroSection />
+    </section>
+
+    <section className='padding'>
+      <PopularProducts />
+    </section>
+
+    <section className='padding'>
+      <SuperQuality />
+    </section>
+
+    <section className='padding-x py-10'>
+      <Services />
+    </section>
+
+    <section className='padding'>
+      <SpecialOffer />
+    </section>
+
+    <section className='bg-blue-100 padding'>
+      <CustomerReview />
+    </section>
+
+    <section className='padding-x sm:py-32 py-16 w-full'>
+      <Subscribe />
+    </section>
+
+    <section className='padding-x padding-t pb-8 bg-black'>
+      <Footer />
+    </section>
+  </main>
+)
 
 export default App
-
